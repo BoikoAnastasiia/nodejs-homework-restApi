d63647e96fad91418c4b74f8ecc2b06a15986924
Anastasiia Boiko <nastasyah86@gmail.com>
2021-06-17 13:51:35 +0300

    works

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,144 +1,139 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const Contacts = require('../../model/contacts');
 const {
-	getContactById,
-	listContacts,
-	removeContact,
-	updateContact,
-	addContact,
-} = require("../../model/contacts.js");
-const { responseHttp } = require("../../helpers/constants");
-const {
-	validationCreatContact,
-	validationUpdateContact,
-	validationObjectId,
-} = require("../validation/validationContacts");
+  validationAddContact,
+  validationUpdateContact,
+  validationUpdateContactFavoriteStatus,
+  validationObjectId,
+} = require('../validation/validationContacts');
 
-const handleError = require ('../../helpers/hendle-error')
+const errorHandler = require('../../helpers/handleError');
 
-router.get("/", async (req, res, next) => {
-	try {
-		const contacts = await listContacts();
-		return res.json({
-			status: "success",
-			code: responseHttp.OK,
-			data: {
-				contacts,
-			},
-		});
-	} catch (error) {
-		next(error);
-	}
+router.get('/', async (req, res, next) => {
+  try {
+    const contacts = await Contacts.listContacts();
+    return res.json({
+      status: 'success',
+      code: 200,
+      data: { contacts },
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get("/:contactId", validationObjectId, async (req, res, next) => {
-	try {
-		const contact = await getContactById(req.params.contactId);
-		if (contact) {
-			return res.json({
-				status: "success",
-				code: responseHttp.OK,
-				data: {
-					contact,
-				},
-			});
-		} else {
-			return res.status(404).json({
-				status: "error",
-				code: responseHttp.NOT_FOUND,
-				data: "Not Found",
-			});
-		}
-	} catch (error) {
-		next(error);
-	}
+router.get('/:contactId', async (req, res, next) => {
+  try {
+    const contact = await Contacts.getContactById(req.params.contactId);
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 201,
+        data: { contact },
+      });
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        data: 'Not found',
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post("/", validationCreatContact, handleError (async (req, res, next) => {
+router.post(
+  '/',
+  validationAddContact,
+  errorHandler(async (req, res, next) => {
+    const contact = await Contacts.addContact(req.body);
+    return res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: { contact },
+    });
+  })
+);
 
-		const contact = await addContact(req.body);
-		return res.status(201).json({
-			status: "success",
-			code: responseHttp.CREATED,
-			data: {
-				contact,
-			},
-		});
-}));
-
-router.delete("/:contactId", async (req, res, next) => {
-	try {
-		const contact = await removeContact(req.params.contactId);
-		if (contact) {
-			return res.json({
-				status: "success",
-				code: responseHttp.OK,
-				data: {
-					message: "contact deleted",
-				},
-			});
-		} else {
-			return res.status(404).json({
-				status: "error",
-				code: responseHttp.NOT_FOUND,
-				data: "Not Found",
-			});
-		}
-	} catch (error) {
-		next(error);
-	}
+router.delete('/:contactId', async (req, res, next) => {
+  try {
+    const contact = await Contacts.removeContact(req.params.contactId);
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 201,
+        message: 'Contact deleted',
+      });
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'Not found',
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.put("/:contactId", validationUpdateContact, async (req, res, next) => {
-	try {
-		if (!req.body) {
-			return res.status(400).json({ message: "missing fields" });
-		}
-		const contact = await updateContact(req.params.contactId, req.body);
-		if (contact) {
-			return res.json({
-				status: "success",
-				code: responseHttp.OK,
-				data: {
-					contact,
-				},
-			});
-		} else {
-			return res.status(404).json({
-				status: "error",
-				code: responseHttp.NOT_FOUND,
-				data: "Not Found",
-			});
-		}
-	} catch (error) {
-		next(error);
-	}
-});
+router.put(
+  '/:contactId',
+  validationObjectId,
+  validationUpdateContact,
+  async (req, res, next) => {
+    try {
+      const contact = await Contacts.updateContact(
+        req.params.contactId,
+        req.body
+      );
+      if (contact) {
+        return res.json({
+          status: 'success',
+          code: 200,
+          data: { contact },
+        });
+      } else {
+        return res.status(404).json({
+          status: 'error',
+          code: 404,
+          data: 'Not found',
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
-router.patch(':contactId/favorite', async (req, res, next) => {
-	try {
-		if (!req.body) {
-			return res.status(400).json({ message: "missing fields favorite" });
-		}
-		const contact = await updateStatusContact(req.params.contactId, req.body);
-		if (contact) {
-			return res.json({
-				status: "success",
-				code: responseHttp.OK,
-				data: {
-					contact,
-				},
-			});
-		} else {
-			return res.status(404).json({
-				status: "error",
-				code: responseHttp.NOT_FOUND,
-				data: "Not Found",
-			});
-		}
-	} catch (error) {
-		next(error);
-	}
-})
+router.patch(
+  '/:contactId',
+  validationObjectId,
+  validationUpdateContactFavoriteStatus,
+  async (req, res, next) => {
+    try {
+      const contact = await Contacts.updateContact(
+        req.params.contactId,
+        req.body
+      );
+      if (contact) {
+        return res.json({
+          status: 'success',
+          code: 201,
+          data: { contact },
+        });
+      } else {
+        return res.status(404).json({
+          status: 'error',
+          code: 404,
+          data: 'Not found',
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 module.exports = router;
