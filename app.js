468e5210d78ea102bbc8dc5bfaa80477a5deceb0
Anastasiia Boiko <nastasyah86@gmail.com>
2021-06-26 19:05:15 +0300

    done

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const HttpCodes = require('./helpers/httpCodes');
 
-const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users');
+const contactsRouter = require('./routes/api/contacts');
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/users', usersRouter);
+app.use('/api/contacts', contactsRouter);
 
 app.use((_req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(HttpCodes.NOT_FOUND).json({ message: 'Not found' });
+});
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+app.use((err, _req, res, _next) => {
+  res
+    .status(err.status || HttpCodes.SERVER_ERROR)
+    .json({ message: err.message || 'unknown error' });
+});
 
-module.exports = app
+module.exports = app;
